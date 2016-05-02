2de0a6051f2865ad01a7076e2c5ade4a791fdebb
Kim T <kim@bynd.com>
2016-05-02 17:22:24 -0400

    styling

--- js/scene.js
+++ js/scene.js
@@ -10,14 +10,14 @@
     var module = {
         init: function () {
             var me = this,
-                light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6),
-                texture = THREE.ImageUtils.loadTexture('img/checker.png'),
+                light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6),
                 geometry = new THREE.PlaneGeometry(1000, 1000);
             this.cannon();
             this.clock = new THREE.Clock();
             this.renderer = new THREE.WebGLRenderer();
-            this.renderer.shadowMapEnabled = true;
+            this.renderer.shadowMap.enabled = true;
             this.renderer.shadowMapSoft = true;
+            this.renderer.setClearColor(0xA5DBFF);
             this.element = this.renderer.domElement;
             this.container = document.getElementById('scene');
             this.container.appendChild(this.element);
@@ -33,23 +33,63 @@
             this.controls.noPan = true;
             this.setControls = this.setOrientationControls.bind(this);
             window.addEventListener('deviceorientation', this.setControls, true);
+            light.position.set(0, 500, 0);
             this.scene.add(light);
-            texture.wrapS = THREE.RepeatWrapping;
-            texture.wrapT = THREE.RepeatWrapping;
-            texture.repeat = new THREE.Vector2(50, 50);
-            texture.anisotropy = this.renderer.getMaxAnisotropy();
+
+            // LIGHTS
+            var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+            hemiLight.color.setHSL( 0.6, 1, 0.6 );
+            hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+            hemiLight.position.set( 0, 500, 0 );
+            this.scene.add( hemiLight );
+            var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+            dirLight.color.setHSL( 0.1, 1, 0.95 );
+            dirLight.position.set(1, 2, 1);
+            dirLight.target.position.set( 0, 0, 0 );
+            dirLight.position.multiplyScalar( 50 );
+            dirLight.castShadow = true;
+            dirLight.shadow.bias = 0.0001;
+            dirLight.shadow.mapSize.width = 2048;
+            dirLight.shadow.mapSize.height = 2048;
+            this.scene.add( dirLight );
+
             this.material = new THREE.MeshPhongMaterial({
                 color: 0xffffff,
                 specular: 0xffffff,
                 shininess: 20,
                 shading: THREE.FlatShading,
-                map: texture
+                map: THREE.ImageUtils.loadTexture('img/metal.jpg')
             });
-            var mesh = new THREE.Mesh(geometry, this.material);
-            mesh.castShadow = true;
-            mesh.receiveShadow = true;
-            mesh.rotation.x = -Math.PI / 2;
-            this.scene.add(mesh);
+
+            this.material2 = new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                specular: 0xffffff,
+                shininess: 2,
+                shading: THREE.FlatShading,
+                map: THREE.ImageUtils.loadTexture('img/ball.jpg')
+            });
+
+            var texture3 = THREE.ImageUtils.loadTexture('img/grass.jpg');
+            texture3.wrapS = THREE.RepeatWrapping;
+            texture3.wrapT = THREE.RepeatWrapping;
+            texture3.repeat = new THREE.Vector2(100, 100);
+            texture3.anisotropy = this.renderer.getMaxAnisotropy();
+
+            this.material3 = new THREE.MeshPhongMaterial({
+                color: 0x666666,
+                specular: 0x000000,
+                shininess: 0,
+                shading: THREE.FlatShading,
+                map: texture3
+            });
+
+            // GROUND
+            var groundGeo = new THREE.PlaneBufferGeometry( 100, 100 );
+            var ground = new THREE.Mesh( groundGeo, this.material3 );
+            ground.rotation.x = -Math.PI/2;
+            ground.receiveShadow = true;
+            this.scene.add( ground );
+
             window.addEventListener('resize', function () {
                 me.resize();
             }, false);
@@ -60,6 +100,7 @@
                 me.fire();
             });
             this.addContent();
+//            this.addObject('objects/geo_ball.obj');
             this.animate();
         },
         cannon: function () {
@@ -83,7 +124,7 @@
             this.time = Date.now();
 
             this.ballShape = new CANNON.Sphere(0.2);
-            this.ballGeometry = new THREE.SphereGeometry(this.ballShape.radius, 32, 32);
+            this.ballGeometry = new THREE.SphereGeometry(this.ballShape.radius, 100, 100);
             this.shootDirection = new THREE.Vector3();
             this.shootVelo = 15;
             this.projector = new THREE.Projector();
@@ -259,7 +300,7 @@
                     y = this.sphereBody.position.y,
                     z = this.sphereBody.position.z,
                     ballBody = new CANNON.Body({mass: 1}),
-                    ballMesh = new THREE.Mesh(this.ballGeometry, this.material);
+                    ballMesh = new THREE.Mesh(this.ballGeometry, this.material2);
                 ballBody.addShape(this.ballShape);
                 this.world.add(ballBody);
                 this.scene.add(ballMesh);
@@ -283,6 +324,36 @@
             this.projector.unprojectVector(vector, this.camera);
             var ray = new THREE.Ray(this.sphereBody.position, vector.sub(this.sphereBody.position).normalize());
             targetVec.copy(ray.direction);
+        },
+        addObject: function (url) {
+            var me = this,
+                manager = new THREE.LoadingManager(),
+                loader = new THREE.OBJLoader(manager),
+                loader2 = new THREE.OBJLoader(manager),
+                texture = new THREE.Texture();
+            manager.onProgress = function (item, loaded, total) {
+                console.log(item, loaded, total);
+            };
+            loader.load('img/grid.jpg', function (image) {
+                texture.image = image;
+                texture.needsUpdate = true;
+            });
+            loader2.load(url, function (object) {
+                object.traverse(function (child) {
+                    if (child instanceof THREE.Mesh) {
+                        child.material.map = texture;
+                    }
+                });
+                object.position.x = 20;
+                object.position.y = 2;
+                object.position.z = 20;
+                var boxBody = new CANNON.Body({ mass: 5 }),
+                    halfExtents = new CANNON.Vec3(1, 1, 1),
+                    boxShape = new CANNON.Box(halfExtents);
+                boxBody.addShape(boxShape);
+                me.world.add(boxBody);
+                me.scene.add(object);
+            });
         }
     };
     module.init();
